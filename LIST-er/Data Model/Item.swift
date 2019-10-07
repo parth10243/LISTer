89c0283a09ebf134bffdbd8175ac3174480b2664
parth10243 <parthmittal10243@gmail.com>
2019-10-08 03:51:29 +1100

    Convert to MVC design pattern

---
+++ LIST-er/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  LIST-er
+//
+//  Created by Parth on 8/10/19.
+//  Copyright © 2019 Parth. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+ }
