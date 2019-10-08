c7761b4ed7d6fafc089f7df00f8ed30107db8494
parth10243 <parthmittal10243@gmail.com>
2019-10-08 15:38:56 +1100

    Persisting data using coddle and plist

--- LIST-er/Controllers/ListerViewController.swift
+++ LIST-er/Controllers/ListerViewController.swift
@@ -11,27 +11,12 @@ import UIKit
 class ListerViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for:  .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+           
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let newItem = Item()
-        newItem.title = "Option1"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Option2"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Option3"
-        itemArray.append(newItem3)
-        
-//         Do any additional setup after loading the view.
-        if let items = defaults.array(forKey: "listItemArray") as? [Item]{
-            itemArray = items
-        }
+        loadItems()
     }
     
     //MARK: Tableview datasource method
@@ -54,8 +39,7 @@ class ListerViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        
-        tableView.reloadData()
+        saveItem()
         tableView.deselectRow(at: indexPath , animated: true)
     }
     
@@ -72,8 +56,8 @@ class ListerViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-              self .defaults.set(self.itemArray, forKey: "listItemArray" )
-            self.tableView.reloadData()
+            
+            self.saveItem()
         }
         alert.addTextField { ( alertTextField) in
             alertTextField.placeholder = "Create new item"
@@ -86,7 +70,34 @@ class ListerViewController: UITableViewController {
     
     }
     
+    func saveItem(){
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch{
+            print("Error encoding item array \(error)")
+        }
+                 
+         self.tableView.reloadData()
+        
+    }
     
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+                itemArray = try decoder.decode([Item].self, from: data)
+        
+            }catch{
+                 print("Decoding error, \(error)")
+            }
+        
+    }
+    
+    
+    }
     
 }
 
